c95fa39c6d909f561466e5ce52c680e7fcd7352c
Meronada749 <meron.essayas@eduvaud.ch>
2025-10-01 11:31:02 +0200

    fix(display):Add bigger fonts to Granted and Denied

--- bracelet-innovation/Display.cpp
+++ bracelet-innovation/Display.cpp
@@ -1,11 +1,13 @@
 #include "Display.h" 
 
 void PrintAccessGood(){
-  u8x8.setFont(u8x8_font_chroma48medium8_r);
-  u8x8.drawString(0,0,"Access Granted"); 
+  u8x8.setFont(u8x8_font_courB18_2x3_r);  // Use a valid font from U8x8 list
+  u8x8.drawString(0, 0, "Access");
+  u8x8.drawString(0, 5, "Granted");
 }
 
-void PrintAccessDenied(){
-  u8x8.setFont(u8x8_font_chroma48medium8_r);
-  u8x8.drawString(0,0,"Access Denied");
+void PrintAccessDenied() {
+  u8x8.setFont(u8x8_font_courB18_2x3_r);  // Use a valid font from U8x8 list
+  u8x8.drawString(0, 0, "Access");
+  u8x8.drawString(0, 5, "Denied");
 }
--- bracelet-innovation/bracelet-innovation.ino
+++ bracelet-innovation/bracelet-innovation.ino
@@ -12,5 +12,6 @@ void setup() {
 
 void loop() {
   PrintAccessGood();
+  //PrintAccessDenied();
   
 }
