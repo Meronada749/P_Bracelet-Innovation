a035698f17ecd181f0f64dfff30a7039b2244527
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2025-10-01 08:45:52 +0200

    feat(display): affichage test

--- program/main/main.ino
+++ program/main/main.ino
@@ -1,9 +1,20 @@
+#include <Arduino.h>
+#include <U8x8lib.h>
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+
+U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE); 
+
 void setup() {
   // put your setup code here, to run once:
-
+  u8x8.begin();
+  u8x8.setPowerSave(0);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  u8x8.setFont(u8x8_font_chroma48medium8_r);
+  u8x8.drawString(0,0,"Salut Nelson"); 
+  delay(2000);
 }
